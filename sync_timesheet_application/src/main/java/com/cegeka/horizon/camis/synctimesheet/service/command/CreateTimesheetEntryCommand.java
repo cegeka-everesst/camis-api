2e3a144e64398383b8b13d0e2198d4c264b1416c
wbryon <ward.bryon@sunnyset.com>
2023-01-16 23:31:34 +0100

    Logging errors on failure

--- sync_timesheet_application/src/main/java/com/cegeka/horizon/camis/synctimesheet/service/command/CreateTimesheetEntryCommand.java
+++ sync_timesheet_application/src/main/java/com/cegeka/horizon/camis/synctimesheet/service/command/CreateTimesheetEntryCommand.java
@@ -28,8 +28,12 @@ public class CreateTimesheetEntryCommand implements SyncCommand {
 
     @Override
     public void execute(TimesheetService timesheetService) {
-        timesheetService.createTimesheetEntry(resourceId, timeCode, workOrder, loggedHoursByDay);
-        logger.info("Updated timesheetLine of employee {} on date {} with workOrder {} and hours {} ", name, loggedHoursByDay.date(), workOrder.value(), loggedHoursByDay.hours());
+        try{
+            timesheetService.createTimesheetEntry(resourceId, timeCode, workOrder, loggedHoursByDay);
+            logger.info("Updated timesheetLine of employee {} on date {} with workOrder {} and hours {} ", name, loggedHoursByDay.date(), workOrder.value(), loggedHoursByDay.hours());
+        }catch (Exception e){
+            logger.error("Error occurred when trying to update timesheetLine of employee {} on date {} with workOrder {} and hours {} ", name, loggedHoursByDay.date(), workOrder.value(), loggedHoursByDay.hours());
+        }
     }
 
     public LoggedHoursByDay loggedHours() {

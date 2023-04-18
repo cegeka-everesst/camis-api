684f0f2ff16c6157f099a861c3f7638726a3859b
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-18 13:16:34 +0200

    SyncCommand: SyncService added

--- sync_timesheet_application/src/main/java/com/cegeka/horizon/camis/synctimesheet/service/command/SyncCommand.java
+++ sync_timesheet_application/src/main/java/com/cegeka/horizon/camis/synctimesheet/service/command/SyncCommand.java
@@ -1,9 +1,12 @@
 package com.cegeka.horizon.camis.synctimesheet.service.command;
 
+import com.cegeka.horizon.camis.sync_logger.SyncLoggerService;
 import com.cegeka.horizon.camis.timesheet.TimesheetService;
 
 public interface SyncCommand {
-    void execute(TimesheetService timesheetService);
+    void execute(TimesheetService timesheetService, SyncLoggerService syncLoggerService);
 
-    default boolean isError() {return false;}
+    default boolean isError() {
+        return false;
+    }
 }

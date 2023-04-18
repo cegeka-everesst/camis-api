7dcceb1bf16995b9f01e6e4994343a0027ca7f82
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-18 16:31:11 +0200

    SyncTimesheetService: Controller added and implemented

--- sync_timesheet_application/src/main/java/com/cegeka/horizon/camis/synctimesheet/service/SyncTimesheetService.java
+++ sync_timesheet_application/src/main/java/com/cegeka/horizon/camis/synctimesheet/service/SyncTimesheetService.java
@@ -1,6 +1,7 @@
 package com.cegeka.horizon.camis.synctimesheet.service;
 
-import com.cegeka.horizon.camis.sync_logger.SyncLoggerService;
+import com.cegeka.horizon.camis.sync_logger.controller.SyncController;
+import com.cegeka.horizon.camis.sync_logger.service.SyncLoggerService;
 import com.cegeka.horizon.camis.synctimesheet.service.command.SyncCommand;
 import com.cegeka.horizon.camis.timesheet.*;
 import org.slf4j.Logger;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.threeten.extra.LocalDateRange;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
@@ -27,45 +29,38 @@ public class SyncTimesheetService {
     private CompareEmployeeService compareEmployeeService;
     @Autowired
     private SyncLoggerService syncLoggerService;
+    @Autowired
+    private SyncController syncController;
 
-    public void sync(List<Employee> inputEmployees) {
+    public void sync(List<Employee> inputEmployees) throws IOException {
+        inputEmployees.stream().flatMap(inputEmployee -> inputEmployee.weeklyTimesheets().stream().map(weeklyTimesheet -> new WeeklyTimesheetToSync(inputEmployee, weeklyTimesheet))).peek(weeklyTimesheetToSync -> {
+            if (!weeklyTimesheetToSync.timesheet().hasMinimumHoursLogged(minimumHoursLogged)) {
+                syncLoggerService.logAndAddSyncRecord("Not syncing inputEmployee " + weeklyTimesheetToSync.employee().name() + " timesheet starting at " + weeklyTimesheetToSync.timesheet().startDate() + " due to less than " + minimumHoursLogged + " hours logged", weeklyTimesheetToSync.employee().name(), weeklyTimesheetToSync.timesheet().startDate(), minimumHoursLogged);
+            }
+        }).filter(weeklyTimesheetToSync -> weeklyTimesheetToSync.timesheet().hasMinimumHoursLogged(minimumHoursLogged)).forEach(weeklyTimesheetToSync -> {
+            Optional<WeeklyTimesheet> camisTimesheetForThatPeriod = retrieveOriginalLogging(weeklyTimesheetToSync);
+            logger.debug("input information of employee {} : {}", weeklyTimesheetToSync.employee().name(), weeklyTimesheetToSync.employee());
+            logger.debug("camis information of employee {} : {}", weeklyTimesheetToSync.employee().name(), camisTimesheetForThatPeriod);
 
-        inputEmployees
-                .stream()
-                .flatMap(inputEmployee -> inputEmployee.weeklyTimesheets().stream().map(weeklyTimesheet -> new WeeklyTimesheetToSync(inputEmployee, weeklyTimesheet)))
-                .peek(weeklyTimesheetToSync -> {
-                    if (!weeklyTimesheetToSync.timesheet().hasMinimumHoursLogged(minimumHoursLogged)) {
-                        syncLoggerService.logAndAddSyncRecord("Not syncing inputEmployee " + weeklyTimesheetToSync.employee().name() + " timesheet starting at " + weeklyTimesheetToSync.timesheet().startDate() + " due to less than " + minimumHoursLogged + " hours logged", weeklyTimesheetToSync.employee().name(), weeklyTimesheetToSync.timesheet().startDate(), minimumHoursLogged);
-                    }
-                })
-                .filter(weeklyTimesheetToSync -> weeklyTimesheetToSync.timesheet().hasMinimumHoursLogged(minimumHoursLogged))
-                .forEach(
-                        weeklyTimesheetToSync -> {
-                            Optional<WeeklyTimesheet> camisTimesheetForThatPeriod = retrieveOriginalLogging(weeklyTimesheetToSync);
-                            logger.debug("input information of employee {} : {}", weeklyTimesheetToSync.employee().name(), weeklyTimesheetToSync.employee());
-                            logger.debug("camis information of employee {} : {}", weeklyTimesheetToSync.employee().name(), camisTimesheetForThatPeriod);
+            List<SyncCommand> syncCommands = compareEmployeeService.compare(weeklyTimesheetToSync.employee(), weeklyTimesheetToSync.timesheet(), camisTimesheetForThatPeriod);
+            if (syncCommands.stream().anyMatch(SyncCommand::isError)) {
+                syncLoggerService.logAndAddSyncRecord("Not syncing inputEmployee " + weeklyTimesheetToSync.employee().name() + " timesheets due to " + syncCommands.stream().filter(SyncCommand::isError).map(SyncCommand::toString).reduce(String::concat).get(), weeklyTimesheetToSync.employee().name());
+            } else {
+                syncCommands.forEach(syncCommand -> syncCommand.execute(timesheetService, syncLoggerService));
+            }
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                logger.error("error while sleeping");
+            }
+        });
+        syncController.sendSyncResult(syncLoggerService.getSyncResultRecordsJson());
 
-                            List<SyncCommand> syncCommands = compareEmployeeService.compare(weeklyTimesheetToSync.employee(), weeklyTimesheetToSync.timesheet(), camisTimesheetForThatPeriod);
-                            if (syncCommands.stream().anyMatch(SyncCommand::isError)) {
-                                syncLoggerService.logAndAddSyncRecord("Not syncing inputEmployee " + weeklyTimesheetToSync.employee().name() + " timesheets due to " + syncCommands.stream().filter(SyncCommand::isError).map(SyncCommand::toString).reduce(String::concat).get(), weeklyTimesheetToSync.employee().name());
-                            } else {
-                                syncCommands.forEach(syncCommand -> syncCommand.execute(timesheetService, syncLoggerService));
-                            }
-                            try {
-                                TimeUnit.SECONDS.sleep(2);
-                            } catch (InterruptedException e) {
-                                logger.error("error while sleeping");
-                            }
-                        }
-                );
         //TODO: retrieve after updates and check correspondences, for example missing holidays
     }
 
     public void retrieve(List<Employee> inputEmployees) {
-        inputEmployees
-                .stream()
-                .flatMap(inputEmployee -> inputEmployee.weeklyTimesheets().stream().map(weeklyTimesheet -> new WeeklyTimesheetToSync(inputEmployee, weeklyTimesheet)))
-                .forEach(this::retrieveOriginalLogging);
+        inputEmployees.stream().flatMap(inputEmployee -> inputEmployee.weeklyTimesheets().stream().map(weeklyTimesheet -> new WeeklyTimesheetToSync(inputEmployee, weeklyTimesheet))).forEach(this::retrieveOriginalLogging);
     }
 
     private Optional<WeeklyTimesheet> retrieveOriginalLogging(WeeklyTimesheetToSync weeklyTimesheetToSync) {

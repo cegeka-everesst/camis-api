ab30fd39b356ea94f8fc0bf2d6c62af7cba01cf7
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-18 13:13:41 +0200

    NoActionSyncCorrect Added: Created basic class

---
+++ sync_logger/src/main/java/com/cegeka/horizon/camis/sync_logger/records/NoActionSyncCorrect.java
@@ -0,0 +1,16 @@
+package com.cegeka.horizon.camis.sync_logger.records;
+
+import com.cegeka.horizon.camis.domain.WorkOrder;
+
+import java.time.LocalDate;
+
+public class NoActionSyncCorrect extends SyncRecord {
+    private final WorkOrder workOrder;
+    private final LocalDate date;
+
+    public NoActionSyncCorrect(String message, String employeeName, LocalDate date, WorkOrder workOrder) {
+        super(message, employeeName);
+        this.workOrder = workOrder;
+        this.date = date;
+    }
+}

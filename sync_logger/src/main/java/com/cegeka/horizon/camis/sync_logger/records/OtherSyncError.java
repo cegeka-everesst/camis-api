4e8ded2c889bd3d7623072cebb7aac1dbc48801e
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-18 13:15:13 +0200

    OtherSyncError Added: Created basic class

---
+++ sync_logger/src/main/java/com/cegeka/horizon/camis/sync_logger/records/OtherSyncError.java
@@ -0,0 +1,7 @@
+package com.cegeka.horizon.camis.sync_logger.records;
+
+public class OtherSyncError extends SyncRecord {
+    public OtherSyncError(String message, String employeeName) {
+        super(message, employeeName);
+    }
+}

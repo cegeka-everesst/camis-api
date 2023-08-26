516d92cdd8463b8b9e229fbbf6a738c1ba91b494
wbryon <ward.bryon@sunnyset.com>
2023-08-26 14:43:07 +0200

    Made the SyncResultType smarter

--- sync_logger/src/main/java/com/cegeka/horizon/camis/sync/logger/model/result/SyncResultType.java
+++ sync_logger/src/main/java/com/cegeka/horizon/camis/sync/logger/model/result/SyncResultType.java
@@ -2,10 +2,16 @@ package com.cegeka.horizon.camis.sync.logger.model.result;
 
 public enum SyncResultType {
 
-    NO_SYNC_REQUIRED,
-    WARNING,
-    INSUFFICIENT_HOURS_ERROR,
-    SUCCESS,
-    OTHER_SYNC_ERROR,
-    UNABLE_TO_UPDATE_TIMESHEET_ERROR
+    NO_SYNC_REQUIRED(false),
+    WARNING (false),
+    INSUFFICIENT_HOURS_ERROR(true),
+    SUCCESS(false),
+    OTHER_SYNC_ERROR(true),
+    UNABLE_TO_UPDATE_TIMESHEET_ERROR(true);
+
+    private final boolean problematic;
+
+    SyncResultType(boolean problematic) {
+        this.problematic = problematic;
+    }
 }

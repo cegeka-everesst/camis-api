dedc49e8c5d7fcf086e109b0f599996435c87116
wbryon <ward.bryon@sunnyset.com>
2023-09-01 09:57:49 +0200

    Fixing small issues

--- sync_logger/src/main/java/com/cegeka/horizon/camis/sync/logger/model/result/HoursInfo.java
+++ sync_logger/src/main/java/com/cegeka/horizon/camis/sync/logger/model/result/HoursInfo.java
@@ -1,12 +1,15 @@
 package com.cegeka.horizon.camis.sync.logger.model.result;
 
 public record HoursInfo(double inputHours, double camisHours) {
+    private static final double NO_HOURS_INFO = -1.0;
+
+
     public static HoursInfo empty() {
-        return new HoursInfo(-1.0, -1.0);
+        return new HoursInfo(NO_HOURS_INFO, NO_HOURS_INFO);
     }
 
     public static HoursInfo inputHours(double inputHours) {
-        return new HoursInfo(0.0, inputHours);
+        return new HoursInfo(inputHours, NO_HOURS_INFO);
     }
 
     public static HoursInfo compare(double inputHours, double camisHours) {
--- sync_timesheet_application/src/main/resources/regex-replace.md
+++ sync_timesheet_application/src/main/resources/regex-replace.md
@@ -1,3 +1,8 @@
 ^(?!.*I011111;Pppppp, Jjjj).*\r?\n
 
+with 
+I011111 = resource Id
+Pppppp = last name
+Jjjj = first name
+
 Replace / Regular Expression On / Replace with "" (nothing)
